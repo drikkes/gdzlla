30a6fb3aef11ac1dc4e09a74cbf24a60de04b72d
Dan Drinkard <dan.drinkard@gmail.com>
2012-12-21 16:03:46 -0500

    add heroku settings file to read stage settings from env

--- config/initializers/01_load_settings.rb
+++ config/initializers/01_load_settings.rb
@@ -1,1 +1,5 @@
-SETTINGS = YAML.load(ERB.new(File.read("#{Rails.root}/config/settings.yml")).result)[Rails.env]
+begin
+  SETTINGS = YAML.load(ERB.new(File.read("#{Rails.root}/config/settings.yml")).result)[Rails.env]
+rescue
+  SETTINGS = YAML.load(ERB.new(File.read("#{Rails.root}/config/settings.heroku.yml")).result)[Rails.env]
+end
