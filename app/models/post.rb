146241786d8bbc435dd4eee358d0a3f436049b1f
Dan Drinkard <dan.drinkard@gmail.com>
2012-12-29 23:30:20 -0500

    missed one

--- app/models/post.rb
+++ app/models/post.rb
@@ -88,7 +88,7 @@ class Post
   end
 
   def thumb_url
-    "http://farm#{flickr_data['farm'].to_s}.static.flickr.com/#{flickr_data['server'].to_s}/#{flickr_data['id'].to_s}_#{flickr_data[:secret]}_s.jpg"
+    "http://farm#{flickr_data['farm'].to_s}.static.flickr.com/#{flickr_data['server'].to_s}/#{flickr_data['id'].to_s}_#{flickr_data['secret']}_s.jpg"
   end
 
   def flickr_url
